b81d9ca2841c03c1afccb391ab8f478014bcd0a6
Maksymilian Moskwytyn <79639894+maksblink@users.noreply.github.com>
2023-05-27 16:49:58 +0200

    Revert "Konrad"

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     public static void main(String[] args) {
         Farmer farmer = new Farmer(100, 0, 0);
@@ -28,5 +29,7 @@ public class Main {
                 System.out.println("\n");
             }
         }
+
+
     }
 }
